ff6baa14558e1e85b3380aafcecf96a816310d81
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2023-05-14 01:38:36 -0300

    configuracoes iniciais de rotas

---
+++ angularSpotifyClone/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from './app.component';
+import { appRotas } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRotas)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularSpotifyClone/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from "@angular/router";
+
+export const appRotas: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(x => x.LoginModule)
+  }
+]
